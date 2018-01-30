4662b5b79a4afcbd5d9dd53372ae7037a49028e3
digitalents <digitalents@digitalentss-MacBook-Air.local>
2018-01-30 13:35:20 +0200

    more type error fixes

--- test/restyle.spec.ts
+++ test/restyle.spec.ts
@@ -6,17 +6,12 @@ import * as path from 'path'
 import { exec } from 'child_process'
 import Restyle from '../bin/src/Restyle'
 import RedditConnection from '../bin/src/RedditConnection'
-import { testFunc } from '../bin/src'
 import * as snoowrap from 'snoowrap'
 import 'mocha'
 
 const command = `node ${path.resolve(__dirname, '../bin/restyle')} watch`
 
 describe('Restyle', function() {
-  it('Should Do Stuff', () => {
-    expect(testFunc()).to.equal('hello')
-  })
-
   it('Should Throw snoowrap NoCredentialsError if Insufficient Credentials', () => {
     const invalidCreds = {
       username: 'username',
@@ -24,13 +19,13 @@ describe('Restyle', function() {
     }
 
     expect(() => new RedditConnection({ invalidCreds } as any)).to.throw(
-      snoowrap.errors.NoCredentialsError
+      (snoowrap as any).errors.NoCredentialsError
     )
   })
 
   it('Should Throw an Error if no Credentials Property (OBJECT)', () => {
     expect(() => new Restyle({ noCredentials: 'moi' })).to.throw(
-      Restyle.errors.NoCredentials
+      (Restyle as any).errors.NoCredentials
     )
   })
 
